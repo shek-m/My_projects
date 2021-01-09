ae2af6fe3ef03e59f9264dd2e5d126e471984bbf
Mykhailo Shekera <jfiekd@gmail.com>
2021-01-09 23:13:41 +0200

    test_2

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("something new");
+        System.out.println("Аня");
     }
 }
