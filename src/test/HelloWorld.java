7745e6095dcfd3e058c4ab0058972d926053a99c
Mykhailo Shekera <jfiekd@gmail.com>
2021-01-09 20:48:09 +0200

    test_2

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -3,5 +3,6 @@ package test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("something new");
     }
 }
